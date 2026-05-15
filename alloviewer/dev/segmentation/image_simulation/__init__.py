a8149530c19d170cbfd65e7ade8ab8d99ac74171
TarikExner <Tarik.Exner@gmail.com>
2026-05-15 22:39:42 +0200

    changed imports

--- alloviewer/dev/segmentation/image_simulation/__init__.py
+++ alloviewer/dev/segmentation/image_simulation/__init__.py
@@ -1,16 +1,34 @@
-from .camera_dimension_config import default_camera, CameraDimension
-from .simulation_config import default_scene, SimulatorConfig
-from .camera_style_config import (
-    diverse_cameras,
-    STYLE_PARAMS_REGISTRY,
-    CameraStyleConfig,
-    CameraStyleParams
-)
 from .histogram_capture import load_or_build_quantile_band_cache
 from .image_simulation import simulate_image
 from .camera_style_application import apply_camera_style
 
+from .simulation_config import (
+    test_scene,
+    train_scene,
+    default_scene,
+    img_export_scene,
+    SimulatorConfig
+)
+from .camera_dimension_config import (
+    default_camera,
+    train_camera,
+    test_camera,
+    img_export_camera,
+    CameraDimension
+)
+from .camera_style_config import (
+    diverse_cameras,
+    simulated_raw_style,
+    phone_mix_style,
+    STYLE_PARAMS_REGISTRY,
+    CameraStyleConfig,
+    CameraStyleParams
+)
+
 __all__ = [
+    "train_scene",
+    "test_scene",
+    "default_scene",
     "simulate_image",
     "STYLE_PARAMS_REGISTRY",
     "load_or_build_quantile_band_cache",
@@ -19,7 +37,13 @@ __all__ = [
     "SimulatorConfig",
     "CameraDimension",
     "default_camera",
+    "train_camera",
+    "test_camera",
+    "img_export_camera",
     "default_scene",
+    "img_export_scene",
     "apply_camera_style",
+    "simulated_raw_style",
+    "phone_mix_style",
     "CameraStyleParams"
 ]
